997181628c9f6f75f1f68ef41e0e5c9bb239452b
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-27 16:00:56 +0100

    change the home page route by fetching some of the chosen characters by their id

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ const base =  "https://doctor-who-api.onrender.com/api";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    try {
+        const characterIds = [10, 11, 12, 13, 17, 24, 20, 21, 22]
+        const responses = await Promise.all(
+            characterIds.map((id) => axios.get(`${base}/character/${id}`))
+        );
+        const specificCharacters =  responses.map((response) => response.data);
+        console.log(specificCharacters);
+        
+        res.render("index.ejs", { characters: specificCharacters });
+    } catch (error) {
+        console.error("Error fetching characters:", error.message);
+        res.status(500).send("An error occurred while fetching characters.");
+    }
+   
 })
 
 
